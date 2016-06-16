45a40e275ad821409ed558248922564b8e638ff1
TheNurse <tdz_7@hotmail.com>
2016-06-16 14:58:56 +0700

    chang var to let and add use strict

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict'; //คำสั่งเปิดใช้
+let day='Monday';
+console.log(day);
+
+console.log('Hello world'); //print//terminal > node filename
+
+/*  var day='Monday';
+console.log(day); */
+
+
+
