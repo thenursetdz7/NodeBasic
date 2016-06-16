ce2ad9a2721a2b610a1b65ff6075f1bff9927fe0
TheNurse <tdz_7@hotmail.com>
2016-06-16 15:31:37 +0700

    arrays

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const student={
     id:'562115028',
     name: 'Nurse'
 };
-console.log('id',student.id);
-console.log('name',student.name);
+const student2={
+    id:'562115003',
+    name: 'Kris'
+};
+const student3={
+    id:'562115000',
+    name: 'Test'
+};
+const students=[student,student2]
+if(Math.random()>0.5){
+    students.push(student3)
+}
+console.log(students);
 
 
 
