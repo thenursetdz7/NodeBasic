d81fe1091d896ae8743ba2fc1692fa0f9caede82
TheNurse <tdz_7@hotmail.com>
2016-06-16 15:10:35 +0700

    const

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict'; //คำสั่งเปิดใช้
-let day='Monday';
+let day='Monday'; 
+day='Tuesday'; //เขัยนทับเลย
+console.log(day);
+
+const d='Monday';//ห้ามเขียนทับกัน คล้ายไฟนอล
 console.log(day);
 
 console.log('Hello world'); //print//terminal > node filename
