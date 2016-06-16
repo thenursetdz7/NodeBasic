7bb4af6bae04f04dad0bab1e102bece9513aa635
TheNurse <tdz_7@hotmail.com>
2016-06-16 15:46:55 +0700

    function

--- index.js
+++ index.js
@@ -11,11 +11,17 @@ const student3={
     id:'562115000',
     name: 'Test'
 };
-const students=[student,student2,student3]
-if(Math.random()>0.5){
-    students.splice(0,1); // ตัด index 0 ถึง ก่อน 1
-}
-console.log(students);
 
 
+const getStudentById=(id)=>{
+    console.log(id);
+
+};
+
+/* const getStudentById= function(id){
+    console.log(id);
+
+};*/
+
+getStudentById(10);
 
