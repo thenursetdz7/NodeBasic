5a9bed276e8d9243654a53f1917d634a087e624b
TheNurse <tdz_7@hotmail.com>
2016-06-16 15:20:26 +0700

    object

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 'use strict';
-let a = null;
-console.log(typeof 'Hello');
-console.log(typeof 1);
-console.log(typeof 0);
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(typeof null);
-console.log('guess','-',a);
+const student={
+    id:'562115028',
+    name: 'Nurse'
+};
+console.log(student);
+
 
 
