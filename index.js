fad909e80891c6eec9f89960da7b6c8bc9afef6a
TheNurse <tdz_7@hotmail.com>
2016-06-18 13:15:52 +0700

    search and filter HW

--- index.js
+++ index.js
@@ -17,20 +17,41 @@ const student3={
 
 const students=[student1,student2,student3];
 
-const getStudentById=(id)=>{
+const getStudentByIds=(id)=> {
+    //students.forEach(student=>{
+
+    students.forEach((student)=> {
+        if(id===student.id)
+            console.log(student)
+    });
+};
+getStudentByIds('562115028')
+const getStudentByName=(name)=> {
+    //students.forEach(student=>{
+
+    students.forEach((student)=> {
+        if(name===student.name)
+            console.log(student)
+    });
+};
+getStudentByName('Test')
+
+const getStudentByGrade=(grade)=>{
     console.log(students
-        //.filter(student=>student.id===id)
-        .map(student=>{
-            return {id:student.id,fullname:student.name+'55'};
-        })//เปลี่ยนรูปแบบ
+        .filter(student=>student.grade===grade)
+            .map(student=>{
+                return {id:student.id,fullname:student.name,grade:student.grade};
+            })//เปลี่ยนรูปแบบ
         //.pop() //เอาอันบนสุดอันเดียว
     );
 };
-const isSomeStudentHasGradeGreaterThan2=()=>{
+/*const isSomeStudentHasGradeGreaterThan2=()=>{
     return students.every(student=> student.grade>2.00);//ทุกคัว
-}
-//getStudentById('562115028');//input id
+}*/
+getStudentByGrade(3.1);//input id
 //console.log(isSomeStudentHasGradeGreaterThan2());
-const numbers=[1,2,,3,4,5,6,7,8,9,10];
+
+
+/*const numbers=[1,2,,3,4,5,6,7,8,9,10];
 const sum=numbers.reduce((total,nextValue)=>total+nextValue,0); //0คือค่าเริ่มต้น
-console.log(sum);
+console.log(sum);*/
